74627c00b2e500004027ccb525eadbeb07713bbd
lljmb <brown.jessm@icloud.com>
2021-02-11 19:43:50 -0500

    still unbreaking

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const dotenv = require('dotenv').config()
+const express = require('express')
 
-console.log('HEY HEY HEY')
-console.log(__dirname)
-console.log(__filename)
-console.log(process.env.USERNAME)
-console.log(process.env.PORT)
+const app = express()
+const port = process.env.PORT || 9000
+
+// /* points to anything after the /
+app.use('/api/*', (_, res) => {
+    res.json({data: 'the api lives'})
+})
+
+app.listen(port, () => {
+    console.log(`server is alive on port: ${port}`)
+})
+
+// console.log('HEY HEY HEY')
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)
+// console.log(process.env.PORT)
