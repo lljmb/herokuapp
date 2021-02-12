e58133c4b24da229432c66ea2ac992ab77dc913b
lljmb <brown.jessm@icloud.com>
2021-02-11 19:11:32 -0500

    unbreaking app

---
+++ index.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv').config()
+
+console.log('HEY HEY HEY')
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USERNAME)
+console.log(process.env.PORT)
